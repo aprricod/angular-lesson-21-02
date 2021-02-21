a7c259567e06636c3da55c60c788f61f7c3f555b
Artem <aprricod@gmail.com>
2021-02-21 09:11:42 +0300

    mod comp added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Mod1Module } from './mod1/mod1.module';
+import { Mod2Module } from './mod2/mod2.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Mod1Module,
+    Mod2Module
   ],
   providers: [],
   bootstrap: [AppComponent]
